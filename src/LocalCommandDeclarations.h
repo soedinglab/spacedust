40acb33cfb884590b28110bade20c643c56fbcf2
RuoshiZ <rosyzhang9701@gmail.com>
2026-01-16 16:28:55 +0100

    Fix seg fault when accessing position 1 in reverse strand, make a local gff2db

--- src/LocalCommandDeclarations.h
+++ src/LocalCommandDeclarations.h
@@ -5,6 +5,7 @@
 
 //extern int easypredict(int argc, const char **argv, const Command& command);
 extern int createsetdb(int argc, const char **argv, const Command& command);
+extern int gff2db(int argc, const char **argv, const Command& command);
 extern int aa2foldseek(int argc, const char **argv, const Command& command);
 extern int clustersearch(int argc, const char **argv, const Command& command);
 extern int clusterhits(int argc, const char **argv, const Command& command);
